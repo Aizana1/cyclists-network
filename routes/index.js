1a44ea885bae0048d8c3d375a26c8f2d1cc00506
Aizana <ayzana2010@gmail.com>
2021-07-11 09:35:07 +0300

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -23,9 +23,13 @@ function transformMapRating(maps) {
   });
 }
 // при переходе на "корень", запускается функция sessionChecker
-router.get("/", async (req, res) => {
+router.get("/",  async (req, res) => {
   let maps = await getMaps();
   maps = transformMapRating(maps)
+  if (req.session.user) {
+    const { user } = req.session
+    return res.render ('landing', { name: user.username, maps })
+  }
   return res.render("landing", { maps });
 });
 
